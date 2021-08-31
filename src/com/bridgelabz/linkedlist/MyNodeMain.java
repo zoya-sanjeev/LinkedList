6c8ce90cd0cb21fb1ea0a5e37968ae459d69ab7d
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-31 19:38:14 +0530

    MODIFIED: used append method to add nodes to linked list

--- src/com/bridgelabz/linkedlist/MyNodeMain.java
+++ src/com/bridgelabz/linkedlist/MyNodeMain.java
@@ -3,13 +3,13 @@ package com.bridgelabz.linkedlist;
 public class MyNodeMain {
 
 	public static void main(String[] args) {
-		MyNode<Integer> firstNode = new MyNode(70);
+		MyNode<Integer> firstNode = new MyNode(56);
 		MyNode<Integer> secondNode = new MyNode(30);
-		MyNode<Integer> thirdNode = new MyNode(56);
+		MyNode<Integer> thirdNode = new MyNode(70);
 		LinkedList linkedList= new LinkedList();
-		linkedList.add(firstNode);
-		linkedList.add(secondNode);
-		linkedList.add(thirdNode);
+		linkedList.append(firstNode);
+		linkedList.append(secondNode);
+		linkedList.append(thirdNode);
 		linkedList.printNodes();
 	}
 
