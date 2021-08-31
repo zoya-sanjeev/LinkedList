5f39d49ad450cf9d525ca77229b93694d60fb5c8
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-31 19:35:30 +0530

    ADDED:append method added to LinkedList class

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -18,6 +18,19 @@ public class LinkedList {
 			newNode.setNext(head);
 			head=newNode;
 	}
+	
+	public void append(INode newNode) {
+		if(this.head==null)
+			this.head=newNode;
+		if(this.tail==null)
+			this.tail=newNode;
+		else {
+			this.tail.setNext(newNode);
+			this.tail=newNode;
+		}
+			
+		
+	}
 	public void printNodes() {
 		INode tempNode = head;
 		while(tempNode!=null) {
